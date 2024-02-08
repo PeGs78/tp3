501a82b263be0682fa4750cde2671373a7e5e2a2
PeGs78 <tom.boulec@gmail.com>
2024-02-08 10:36:21 +0100

    fin video 9

--- supprimerNationalite.php
+++ supprimerNationalite.php
@@ -5,30 +5,12 @@ $req=$monPdo->prepare("delete from nationalite where num = :num");
 $req->bindParam(':num', $num);
 $nb=$req->execute();
 
-
-echo'<div class="container mt-5">';
-echo '<div class="row">
-    <div class="col mt-3">';
 if($nb == 1){
-    echo '<div class="alert alert-primary" role="alert">
-        La nationalité a bien été supprimée
-        </div>';
+    $_SESSION['message']=['primary'=>"La nationalité a bien été supprimée"];
 }else{
-    echo '<div class="alert alert-danger" role="alert">
-    La nationalité n\'a pas été supprimée !
-    </div>';
+    $_SESSION['message']=['danger' => 'la nationalité n\'a pas étè supprimée'];
 }
-?>
-</div>
-</div>
-<a href="listeNationalites.php" class="btn btn-primary">Revenir a la liste des nationalités</a>
-
-
-<div class="container mt-5">
-
-</div>
-
-
-<?php include "footer.php";
+header('location: listeNationalites.php');
+exit();
 
 ?>
